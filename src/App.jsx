bf323b4c9fc7438ac3971a689382dfd6eb6c7ea6
smalldevK <wwwmnt@naver.com>
2025-06-12 14:07:08 +0900

    firshCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-
+import { Route, Routes } from 'react-router'
+import FeedPage from './pages/user/socialRing/FeedPage';
+import UserFeed from './pages/user/socialRing/UserFeed';
+import FeedDetail from './pages/user/socialRing/FeedDetail';
+import Sidebar from './pages/user/socialRing/Sidebar';
 function App() {
 
   return (
     <>
-      {/* 공통 /~~~으로 시작 */}
+    <Routes>
 
+      {/* 공통 /~~~으로 시작 */}
+      <Route path="/feed" element={<FeedPage/>}/>
+      <Route path="/userFeed" element={<UserFeed/>}/>
+      <Route path="/feedDetail" element={<FeedDetail/>}/>
+      <Route path="/sidebar" element={<Sidebar/>}/>
       {/* 로그인한 유저 /user/~~~ */}
 
       {/* 유저의 마이페이지 /user/mypage/~~~~ */}
@@ -15,6 +24,8 @@ function App() {
       
       {/* 관리자 /admin/~~~~ */}
 
+      
+    </Routes>
     </>
   )
 }

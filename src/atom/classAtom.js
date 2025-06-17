739081e4672e0fe16f90c427722df0d77a5889d5
s0ycraft <s0ycr4ft@gmail.com>
2025-06-17 20:44:50 +0900

    category 수정

--- src/atom/classAtom.js
+++ src/atom/classAtom.js
@@ -4,3 +4,6 @@ export const recommendClassAtom = atom([]);
 export const hotClassAtom = atom([]);
 export const recommendGatheringAtom = atom([]);
 export const mainBannerList = atom([]);
+export const userClassList = atom([]);
+export const category = atom([]);
+export const subCategory = atom([]);

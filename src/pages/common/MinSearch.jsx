16cf2fd3a2e22c4a3d096968409c97de4be75ada
osk0521 <sangkyung21@naver.com>
2025-07-15 15:45:37 +0900

    CSS. 찐막

--- src/pages/common/MinSearch.jsx
+++ src/pages/common/MinSearch.jsx
@@ -119,7 +119,7 @@ export default function MainSearch() {
                                         <span className={`type-text ${item.type}`}>{getLabel(item.type)} | </span>
                                         {truncate(item.title, 30)}
                                     </div>
-                                    <br />
+                                    {/* <br /> */}
                                     {item.content && (
                                         <div className="card-content-text viewer-wrapper">
                                             {item.content.includes('<') || item.content.includes('>') ? (
